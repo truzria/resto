5e66560181cd9fd153d9cfeb78d6b6e4bc8bd4c6
maria <truzria@outlook.com>
2024-10-21 20:35:26 +0800

    Added path to styles folder

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import path from 'path';
 const app = express();
 const port = 3000;
 
+app.use('/styles', express.static(path.join(process.cwd(), 'frontend', 'styles')));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(process.cwd(), 'frontend', 'index.html'));
 });
